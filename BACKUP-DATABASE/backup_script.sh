06ba344331d8c394e0984dbb3946639bfcd8e458
s7testar <attamegnon@gmail.com>
2024-02-08 01:17:18 +0200

    update backup

--- BACKUP-DATABASE/backup_script.sh
+++ BACKUP-DATABASE/backup_script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Create backup file
-pg_dump -h $PG_HOST -p $PG_PORT -U $PG_USERNAME -d $PG_DATABASE --password=$PG_PASSWORD | gzip > $BACKUP_DIR/backup_$(date +%Y%m%d%H%M%S).tar.gz
+# Create backup file 
+ pg_dump -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE | gzip > /backup/db_backup.tar.gz
+
 
 # Upload backup to AWS S3, del to auriole/ is your s3 bucket name
-aws s3 cp $BACKUP_DIR/ s3://del-db-backup/s6/s7auriole/ --recursive
+ aws s3 cp /backup/db_backup.tar.gz  s3://del-db-backup/S6/s7auriole/ 
+
 
